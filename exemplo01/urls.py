a44392fb57127cc78fb0990e2484a5430c86890f
Maycon Spirlandelli <mayconbatista8@gmail.com>
2024-12-28 00:48:55 +0000

    Adicionado CreateView para criacao de formulario

--- exemplo01/urls.py
+++ exemplo01/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
  path('pagina1', views.pagina1, name='pagina1'),
  path('pagina2', views.pagina2, name='pagina2'),
  path('pagina3', views.pagina3, name='pagina3'),
+ path('pessoa_create/', views.pessoa_create.as_view, name='pessoa_create_alias'),
 ]
--- exemplo01/views.py
+++ exemplo01/views.py
@@ -23,3 +23,15 @@ def pagina3 (request):
     registros = pessoa.objects.all()
     dicionario['pessoas'] = registros
     return render(request, 'pagina3.html', dicionario)
+
+
+from django.views.generic import ListView
+from .models import pessoa
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+class pessoa_create(CreateView):
+    from .models import pessoa
+    model = pessoa
+    fields = ['nome', 'email', 'celular', 'funcao', 'nascimento', 'ativo']
+    def get_sucess_url(self):
+        return reverse_lazy('pagina3.html')
