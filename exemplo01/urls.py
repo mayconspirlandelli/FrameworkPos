09ae911bb3681cf3622be6964ae3a45704f8903c
Maycon Spirlandelli <mayconbatista8@gmail.com>
2025-01-10 20:10:05 +0000

    Adicionado a tela de login pra autenticação.

--- exemplo01/urls.py
+++ exemplo01/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
- path('', views.index, name='index'),   
+ path('', views.index, name='index_alias'),   
  path('pagina0', views.pagina0, name='pagina0'),
  path('pagina1', views.pagina1, name='pagina1'),
  path('pagina2', views.pagina2, name='pagina2'),
--- exemplo01/views.py
+++ exemplo01/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render 
 from django.http import HttpResponse 
 
-def index (request):
-    return HttpResponse ("EXEMPLO 01.")
+from django.contrib.auth import authenticate, login, logout
+def index(request):
+    print("else")
+    usuario = request.POST.get('username')
+    senha = request.POST.get('password')
+    user = authenticate(username=usuario, password=senha)
+    if (user is not None):
+
+        login(request, user)
+        request.session['username'] = usuario
+        request.session['password'] = senha
+        request.session['usernamefull'] = user.get_full_name()
+        print(request.session['username'])
+        print(request.session['password'])
+        print(request.session['usernamefull'])
+        from django.shortcuts import redirect
+        return redirect('menu_alias')
+    else:
+        return render(request, 'index.html')
 
 def pagina0 (request):
     return render(request, 'pagina0.html')    
